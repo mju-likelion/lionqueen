b1151999f82785b209c2f1131fddc4385bac2a6b
YeonBeen Park <ypyp66@naver.com>
2022-05-03 16:49:08 +0900

    fix: test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Button from "../DesignSystem/Button";
+import Button from "~DesignSystem/Button";
 
 const Home: NextPage = () => {
   return (
