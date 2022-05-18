d0be2355f6830e27d3728837ac8f4c9ec36efaf3
yuraup <o721@naver.com>
2022-05-18 21:39:23 +0900

    docs: test

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -7,7 +7,7 @@ function NavBar() {
       <p className="mb-[71px] mt-8 font-light text-7xl ">메뉴</p>
       <button className="mb-[54px]">마이페이지</button>
       <button className="mb-[54px]">그룹 전환</button>
-      <button className="mb-[54px]">로그아웃</button>
+      <button className="mb-[54px]">로그아웃</button> 
     </div>
   );
 }
