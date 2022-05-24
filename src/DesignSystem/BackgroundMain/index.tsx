016471828e630496eed4855294f96deb1eb345cd
Bhanjo <hanjo1515@naver.com>
2022-05-24 22:01:22 +0900

    feat: 메인 백그라운드 배경 추가

--- src/DesignSystem/BackgroundMain/index.tsx
+++ src/DesignSystem/BackgroundMain/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/style-prop-object */
 import type { ComponentPropsWithoutRef } from 'react';
-import React from 'react';
 import { cls, getClassNames } from '~lib/utils';
+import Wallpaper from '../../../public/image/wallpaper.svg';
+import Trees from '../../../public/image/trees.svg';
 
 type BackgroundProps = {
   // eslint-disable-next-line react/require-default-props
@@ -10,15 +12,37 @@ type BackgroundProps = {
 const BackgroundMain = (props: BackgroundProps) => {
   const { className = '', children, ...restProps } = props;
 
-  const styles = cls(
+  const pageContainer = cls('min-h-screen', 'bg-primary-skyblue', 'flex', 'items-center');
+  const background = cls('min-h-[1024px]', 'min-w-full', 'relative', 'flex', 'items-center');
+
+  const backgroundImage = cls(
+    'absolute',
+    'left-[50%]',
+    'top-[50%]',
+    'translate-x-[-50%]',
+    'translate-y-[-50%]',
+  );
+
+  const tree = cls(
+    'absolute',
+    'left-[50%]',
+    'top-[115%]',
+    'translate-x-[-50%]',
+    'translate-y-[-115%]',
+  );
+
+  const board = cls(
     'w-[1014px]',
     'h-[708px]',
     'bg-primary-yellow',
     'border-[10px]',
     'border-[#ffbb54]',
     'rounded-[30px]',
-    'relative',
-    getClassNames(className),
+    'absolute',
+    'left-[50%]',
+    'top-[30%]',
+    'translate-x-[-50%]',
+    'translate-y-[-30%]',
   );
 
   const lineContainer = cls(
@@ -30,16 +54,10 @@ const BackgroundMain = (props: BackgroundProps) => {
     'right-[25%]',
     'mx-auto',
     'justify-between',
-    getClassNames(className),
   );
-  const lineStyles = cls(
-    'w-[16px]',
-    'h-[118px]',
-    'rounded-[10px]',
-    'bg-[#cd853f]',
-    'pb-[10px]',
-    getClassNames(className),
-  );
+
+  const lineStyles = cls('w-[16px]', 'h-[118px]', 'rounded-[10px]', 'bg-[#cd853f]', 'pb-[10px]');
+
   const contentContainer = cls(
     'w-[912px]',
     'h-[542px]',
@@ -51,12 +69,20 @@ const BackgroundMain = (props: BackgroundProps) => {
   );
 
   return (
-    <div className={styles} {...restProps}>
-      <div className={lineContainer}>
-        <div className={lineStyles} />
-        <div className={lineStyles} />
+    <div className={pageContainer}>
+      <div className={background}>
+        <Wallpaper className={backgroundImage} />
+        <div className={board}>
+          <div className={lineContainer}>
+            <div className={lineStyles} />
+            <div className={lineStyles} />
+          </div>
+          <div className={contentContainer} {...restProps}>
+            {children}
+          </div>
+          <Trees className={tree} />
+        </div>
       </div>
-      <div className={contentContainer}>{children}</div>
     </div>
   );
 };
