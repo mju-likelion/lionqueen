5d40ea28ea9ee344ccdaa88fb0787f9c24e266a1
Bhanjo <hanjo1515@naver.com>
2022-05-25 21:13:49 +0900

    feat: 색상 테마 생성

---
+++ src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    text: '#2e2e2e',
+    placeholder: '#646464',
+    primary: {
+      skyblue: '#c5e5ff',
+      brown: '#8d8255',
+      yellow: '#ffe161',
+      orange: '#ffbb17',
+      green: '#458d10',
+      lightgreen: '#92cd00',
+      error: '#FF7C9C',
+    },
+  },
+};
