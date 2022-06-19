c4014ee62dc120f05aded86644f3cf8913951bbd
qkrthdus605 <qkrthdus605@gmail.com>
2022-06-19 22:56:29 +0900

    feat: 라운지 생성 및 선택 컴포넌트 생성

---
+++ src/pages/LoungeSelect/index.tsx
@@ -0,0 +1,11 @@
+import BackgroundMain from '../../DesignSystem/BackgroundMain';
+
+const LoungeSelect = () => {
+  return (
+    <BackgroundMain>
+      <p>라운지 선택 및 생성 컴포넌트</p>
+    </BackgroundMain>
+  );
+};
+
+export default LoungeSelect;
