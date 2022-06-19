ed738a398ba01063c17d20426dfd28778e882d72
yuraup <o721@naver.com>
2022-06-19 21:20:54 +0900

    style: 마이페이지 UI

--- src/MyPage/index.tsx
+++ src/MyPage/index.tsx
@@ -1,9 +1,42 @@
+import styled from 'styled-components';
 import BackgroundMain from '../DesignSystem/BackgroundMain';
+import Button from '../DesignSystem/Button';
 
+const MypageBg = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TitleStyle = styled.p`
+  font-size: 45px;
+  margin-top: 70px;
+  margin-bottom: 23px;
+`;
+const ButtonStyle = styled.p`
+  margin-top: 28px;
+`;
 const MyPage = () => {
   return (
     <BackgroundMain>
-      <p>마이페이지</p>
+      <MypageBg>
+        <TitleStyle>마이페이지</TitleStyle>
+        <ButtonStyle>
+          <Button color="primary-orange" size="large">
+            나의 정보
+          </Button>
+        </ButtonStyle>
+        <ButtonStyle>
+          <Button color="primary-orange" size="large">
+            라운지 설정
+          </Button>
+        </ButtonStyle>
+        <ButtonStyle>
+          <Button color="primary-orange" size="large">
+            라운지 검색 / 추천
+          </Button>
+        </ButtonStyle>
+      </MypageBg>
     </BackgroundMain>
   );
 };
