b19353be19d48f9ad9002e7c5309b79d4f2c5b53
yuraup <o721@naver.com>
2022-06-20 00:25:23 +0900

    feat: 마이페이지 이중 모달 UI 추가

---
+++ src/pages/MyPage/LoungeLeavingModal.tsx
@@ -0,0 +1,24 @@
+import { Router } from 'next/router';
+import React from 'react';
+import ConfirmModal from '~/components/ConfirmModal';
+
+function LoungeLeavingModal() {
+  return (
+    <ConfirmModal
+      isSingle={false}
+      size="medium"
+      title="소속 라운지 탈퇴"
+      onClose={() => {
+        console.log('취소');
+      }}
+      onConfirm={() => {
+        console.log('확인');
+      }}
+    >
+      정말 그룹이름을 탈퇴하시겠습니까?
+      {/* 정말 {그룹이름}을 탈퇴하시겠습니까? */}
+    </ConfirmModal>
+  );
+}
+
+export default LoungeLeavingModal;
