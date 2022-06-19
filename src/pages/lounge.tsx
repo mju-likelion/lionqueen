54eb957b017bc4db1903b199035eb2fc287f62eb
JJongsKim <totnwjd530@gmail.com>
2022-06-19 13:47:42 +0900

    feat: 라운지 페이지 추가

--- src/pages/lounge.tsx
+++ src/pages/lounge.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import DoorBottom from '../../public/image/DoorBottom.svg';
+import LoungeDoor from '~DesignSystem/LoungeDoor';
 
 const LoungeBg = styled.div`
   display: flex;
@@ -18,84 +19,72 @@ const LoungeList = styled.div`
   justify-content: flex-end;
   width: 1024px;
   height: 100%;
+  overflow: hidden;
   /* background-color: yellowgreen; */
 `;
 
-const LoungeFloor = styled.div`
-  display: flex;
-  flex-direction: column;
+// const LoungeFloor = styled.div`
+//   display: flex;
+//   flex-direction: column;
 
-  .doorContainer {
-    display: flex;
-    justify-content: space-around;
-  }
-`;
+//   .doorContainer {
+//     display: flex;
+//     justify-content: space-evenly;
+//   }
+// `;
 
-const LoungeDoor = styled.div`
-  display: grid;
-  grid-template-rows: repeat(2, 1fr);
-  width: 154px;
-  height: 220px;
-  border-radius: 15px 15px 0 0;
-  box-shadow: 8px 0 5px #7f7f7fb3;
-  background-color: #ffe161;
+// const LoungeDoor = styled.div`
+//   display: grid;
+//   grid-template-rows: repeat(2, 1fr);
+//   width: 154px;
+//   height: 220px;
+//   border-radius: 15px 15px 0 0;
+//   box-shadow: 8px 0 5px #7f7f7fb3;
+//   background-color: #ffe161;
+//   cursor: pointer;
 
-  .nameSpace {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+//   .nameSpace {
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
 
-    p {
-      width: 122px;
-      height: 42px;
-      margin: 0;
-      padding: 0;
-      border-radius: 10px;
-      text-align: center;
-      box-sizing: border-box;
-      background-color: #fff;
-    }
-  }
+//     p {
+//       width: 122px;
+//       height: 42px;
+//       margin: 0;
+//       padding: 0;
+//       border-radius: 10px;
+//       text-align: center;
+//       box-sizing: border-box;
+//       background-color: #fff;
+//     }
+//   }
 
-  .knobContainer {
-    .doorKnob {
-      width: 18px;
-      height: 18px;
-      margin: 0 0 0 10px;
-      border-radius: 100%;
-      box-shadow: 0 4px 5px #7f7f7f99;
-      background-color: #7f7f7f;
-    }
-  }
-`;
+//   .knobContainer {
+//     .doorKnob {
+//       width: 18px;
+//       height: 18px;
+//       margin: 0 0 0 10px;
+//       border-radius: 100%;
+//       box-shadow: 0 4px 5px #7f7f7f99;
+//       background-color: #7f7f7f;
+//     }
+//   }
+// `;
 
-const DoorLine = styled.div`
-  width: 1024px;
-  height: 20px;
-  background-color: #8d8255;
-`;
+// const DoorLine = styled.div`
+//   width: 1024px;
+//   height: 20px;
+//   background-color: #8d8255;
+// `;
 
-const TestDoor = [1, 2, 3, 4];
+// const TestDoor = [1, 2, 3, 4];
 
 const Lounge = () => {
   return (
     <LoungeBg>
       <LoungeList>
-        <LoungeFloor>
-          <div className="doorContainer">
-            {TestDoor.map(item => (
-              <LoungeDoor key={item}>
-                <div className="nameSpace">
-                  <p>유저이름</p>
-                </div>
-                <div className="knobContainer">
-                  <div className="doorKnob" />
-                </div>
-              </LoungeDoor>
-            ))}
-          </div>
-          <DoorLine />
-        </LoungeFloor>
+        <LoungeDoor />
       </LoungeList>
       <DoorBottom />
     </LoungeBg>
