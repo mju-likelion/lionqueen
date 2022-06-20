8217aff0fb2ae5a02a90438232b7cb2798a41345
yuraup <o721@naver.com>
2022-06-20 23:29:56 +0900

    feat: 로딩페이지 컴포넌트 생성

---
+++ src/pages/loadingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+import BackgroundMain from '../DesignSystem/BackgroundMain';
+
+const loadingPage = () => {
+  return (
+    <BackgroundMain>
+      <p>안녕</p>
+    </BackgroundMain>
+  );
+};
+export default loadingPage;
