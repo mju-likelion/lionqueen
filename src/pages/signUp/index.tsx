c897fc651200e9a2460273a7c49fd7e0c5bb9a87
seongyeon <ysy2565@naver.com>
2022-06-21 00:42:25 +0900

    feat: 회원가입 컴포넌트 생성

---
+++ src/pages/signUp/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import BackgroundMain from '~DesignSystem/BackgroundMain';
+
+const signUp = () => {
+  return <BackgroundMain>회원가입UI</BackgroundMain>;
+};
+
+export default signUp;
