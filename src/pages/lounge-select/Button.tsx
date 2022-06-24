a9d1f386aa0319498160bb0ef75a41764a60aadd
qkrthdus605 <qkrthdus605@gmail.com>
2022-06-25 00:47:36 +0900

    feat: button 컴포넌트 생성 및 라운지 선택 UI 완성

---
+++ src/pages/lounge-select/Button.tsx
@@ -0,0 +1,54 @@
+// 물방울 버튼
+import styled from 'styled-components';
+
+import { theme } from '../../styles/theme';
+
+import Bubble from '../../../public/image/Bubble.svg';
+import Left from '../../../public/image/leftarrow.svg';
+import Right from '../../../public/image/rightarrow.svg';
+
+const Button = () => {
+  return (
+    <Container>
+      <ArrowWrapper>
+        <Left />
+      </ArrowWrapper>
+      <BubbleWrapper>
+        <Bubble />
+      </BubbleWrapper>
+      <BubbleWrapper>
+        <Bubble />
+      </BubbleWrapper>
+      <BubbleWrapper>
+        <Bubble />
+      </BubbleWrapper>
+      <ArrowWrapper>
+        <Right />
+      </ArrowWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: 124px;
+  width: 612px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 65px 0 60px 0;
+`;
+
+const BubbleWrapper = styled.div`
+  width: 124px;
+  height: 124px;
+  cursor: pointer;
+`;
+
+const ArrowWrapper = styled.div`
+  width: 49px;
+  height: 55px;
+  cursor: pointer;
+`;
+
+export default Button;
--- src/pages/lounge-select/index.tsx
+++ src/pages/lounge-select/index.tsx
@@ -1,11 +1,61 @@
+import styled from 'styled-components';
+
+import { theme } from '../../styles/theme';
+
 import BackgroundMain from '../../DesignSystem/BackgroundMain';
+import Button from './Button';
+import Plus from '../../../public/image/plus.svg';
 
 const LoungeSelect = () => {
   return (
     <BackgroundMain>
-      <p>라운지 선택</p>
+      <Container>
+        <TitleContainer>
+          <Title>라운지 선택</Title>
+          <Line />
+        </TitleContainer>
+        <Button />
+        <PlusWrapper>
+          <Plus />
+        </PlusWrapper>
+      </Container>
     </BackgroundMain>
   );
 };
+
+const Container = styled.div`
+  width: 612px;
+  height: 542px;
+  display: flex;
+  text-align: center;
+  margin: 0 auto;
+  padding-top: 40px;
+  flex-direction: column;
+`;
+
+const TitleContainer = styled.div`
+  width: 496px;
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+`;
+
+const Title = styled.p`
+  font-size: 36px;
+  margin-top: 58px;
+  margin-bottom: 20px;
+  color: ${({ theme }) => theme.colors.primary.black};
+`;
+
+const Line = styled.div`
+  border-top: 2px solid ${({ theme }) => theme.colors.black};
+`;
+
+const PlusWrapper = styled.div`
+  width: 50px;
+  height: 50px;
+  margin: 0 auto;
+`;
 
 export default LoungeSelect;
