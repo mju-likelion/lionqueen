98c8f3fa044118fc58b8d3a3ef9b8924eb8ca42f
yuraup <o721@naver.com>
2022-06-25 00:41:44 +0900

    feat: 코드리뷰 반영

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -3,6 +3,26 @@ import styled from 'styled-components';
 import BackgroundMain from '../../DesignSystem/BackgroundMain';
 import Button from '../../DesignSystem/Button';
 
+const MyPage = () => {
+  return (
+    <BackgroundMain>
+      <MypageBg>
+        <TitleStyle>마이페이지</TitleStyle>
+
+        <ButtonStyle>
+          {/* <Link href="/MyPage/AccountWithdrawalModal"> */}
+          <ButtonMargin size="large">나의 정보</ButtonMargin>
+          {/* </Link> */}
+          {/* mvp 제외 기능 */}
+          <ButtonMargin size="large">라운지 설정</ButtonMargin>
+          {/* mvp 제외 기능 */}
+          <ButtonMargin size="large">라운지 검색 / 추천</ButtonMargin>
+        </ButtonStyle>
+      </MypageBg>
+    </BackgroundMain>
+  );
+};
+
 const MypageBg = styled.div`
   display: flex;
   flex-direction: column;
@@ -15,37 +35,16 @@ const TitleStyle = styled.p`
   margin-top: 70px;
   margin-bottom: 23px;
 `;
+
 const ButtonStyle = styled.p`
   margin-top: 28px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 28px;
 `;
-const MyPage = () => {
-  return (
-    <BackgroundMain>
-      <MypageBg>
-        <TitleStyle>마이페이지</TitleStyle>
 
-        <ButtonStyle>
-          {/* <Link href="/MyPage/AccountWithdrawalModal"> */}
-          <Button color="primary-orange" size="large">
-            나의 정보
-          </Button>
-          {/* </Link> */}
-        </ButtonStyle>
-        <ButtonStyle>
-          {/* mvp 제외 기능 */}
-          <Button color="primary-orange" size="large">
-            라운지 설정
-          </Button>
-        </ButtonStyle>
-        <ButtonStyle>
-          {/* mvp 제외 기능 */}
-          <Button color="primary-orange" size="large">
-            라운지 검색 / 추천
-          </Button>
-        </ButtonStyle>
-      </MypageBg>
-    </BackgroundMain>
-  );
-};
+const ButtonMargin = styled(Button)`
+  margin-bottom: 28px;
+`;
 
 export default MyPage;
