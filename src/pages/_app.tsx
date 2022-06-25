69dd967808bd67d0e9ec5ae6f93d3b4c610e5143
JJongsKim <totnwjd530@gmail.com>
2022-06-26 00:44:27 +0900

    fix: ThemeProvider 파일 위치 변경하기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
 import { wrapper } from '../store';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
