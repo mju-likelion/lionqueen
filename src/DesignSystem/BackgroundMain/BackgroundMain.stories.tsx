3c9ed568408f011df45be88e99da57d1ee0f17af
YeonBeen Park <ypyp66@naver.com>
2022-06-26 01:35:06 +0900

    fix: 공통 컴포넌트 ThemeProvider 제거 및 스토리북에만 추가

--- src/DesignSystem/BackgroundMain/BackgroundMain.stories.tsx
+++ src/DesignSystem/BackgroundMain/BackgroundMain.stories.tsx
@@ -2,27 +2,21 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Button from '.';
+import BackgroundMain from '.';
 import { theme } from '~/styles/theme';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
+  title: 'Example/BackgroundMain',
+  component: BackgroundMain,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-} as ComponentMeta<typeof Button>;
+} as ComponentMeta<typeof BackgroundMain>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = args => (
+const Template: ComponentStory<typeof BackgroundMain> = args => (
   <ThemeProvider theme={theme}>
-    <Button {...args} />
+    <BackgroundMain {...args} />
   </ThemeProvider>
 );
-export const Orange = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Orange.args = {
-  type: 'button',
-  size: 'small',
-  color: 'primary-orange',
-  children: '버튼',
-};
+
+export const Default = Template.bind({});
