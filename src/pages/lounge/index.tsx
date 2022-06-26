d0d72ccd4764c9ca9edc08863ae25ec66aa5f407
JJongsKim <totnwjd530@gmail.com>
2022-06-27 00:12:33 +0900

    feat: 나눔바른고딕 폰트 추가, 층수 표시 글꼴 변경

--- src/pages/lounge/index.tsx
+++ src/pages/lounge/index.tsx
@@ -80,7 +80,8 @@ const FloorNum = styled.p`
   width: 65px;
   height: 67px;
   margin: 230px 0 0 -50px;
-  font-family: BlinkMacSystemFont;
+  padding: 20px 0 10px 0;
+  font-family: NanumBarunGothic;
   font-size: 40px;
   font-weight: 700;
   text-align: center;
