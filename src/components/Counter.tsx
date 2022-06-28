90e2a903224a49030e882c8e20d4513a9388badf
qkrthdus605 <qkrthdus605@gmail.com>
2022-06-29 00:00:20 +0900

    feat: 인원수 counter기능 구현

---
+++ src/components/Counter.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+import { useState } from 'react';
+import Plus from '../components/icons/ModalPlus';
+import Minus from '../components/icons/Minus';
+
+const Counter = () => {
+  const [count, setCount] = useState<number>(100);
+
+  const incNum = () => {
+    if (count === 100) {
+      setCount(100);
+    } else {
+      setCount(count + 5);
+    }
+  };
+
+  const decNum = () => {
+    if (count === 0) {
+      setCount(0);
+    } else {
+      setCount(count - 5);
+    }
+  };
+
+  return (
+    <Container>
+      <ButtonWrapper onClick={decNum}>
+        <Minus />
+      </ButtonWrapper>
+      {count}
+      <ButtonWrapper onClick={incNum}>
+        <Plus />
+      </ButtonWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 120px;
+  height: 30px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 40px;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 30px;
+  height: 30px;
+  margin: 0 25px 10px 20px;
+`;
+
+export default Counter;
