29e10554a6149c987dbb3fdb42d240c74d8858c9
yuraup <o721@naver.com>
2022-06-28 16:06:55 +0900

    feat: 이중모달 추가

--- src/pages/my-page/index.tsx
+++ src/pages/my-page/index.tsx
@@ -4,16 +4,16 @@ import BackgroundMain from '~/DesignSystem/BackgroundMain';
 import Button from '~/DesignSystem/Button';
 import MyInfoModal from '~/components/MyPage/MyInfoModal';
 import LoungeList from '~/components/MyPage/LoungeList';
+import ConfirmModal from '~/components/ConfirmModal';
 
 const MyPage = () => {
   const [isModalShow, setIsModalShow] = useState(false);
-  const [secondModalShow, setSecondModalShow] = useState(false);
-
+  const [LoungeOutModalShow, setLoungeOutModalShow] = useState(false);
+  const [WithdrawalModalShow, setWithdrawalModalShow] = useState(false);
   return (
     <BackgroundMain>
       <MypageBg>
         <Title>마이페이지</Title>
-
         <MyPageButton>
           <ButtonMargin size="large" onClick={() => setIsModalShow(true)}>
             나의 정보
@@ -23,7 +23,6 @@ const MyPage = () => {
           {/* mvp 제외 기능 */}
           <ButtonMargin size="large">라운지 검색 / 추천</ButtonMargin>
         </MyPageButton>
-
         {isModalShow && (
           <MyInfoModal
             isSingle
@@ -35,27 +34,68 @@ const MyPage = () => {
           >
             <InfoBox>
               <MyInfoTitle>내 정보</MyInfoTitle>
-              <NameBox>
-                <NameTitle>이름</NameTitle>
-                <NameInfo>
-                  <NameText>여기는 이름이 들어옴</NameText>
-                  <NameSaveButton>저장</NameSaveButton>
-                </NameInfo>
-              </NameBox>
+              <NameTitle>이름</NameTitle>
+              <NameInfo>
+                <NameText>여기는 이름이 들어옴</NameText>
+                <NameSaveButton>저장</NameSaveButton>
+              </NameInfo>
               <LoungeBox>
                 <LoungeTitle>소속 라운지</LoungeTitle>
                 <LoungeInfo>
                   {LoungeList.map(lounge => (
                     <LoungeRow>
                       <LoungeName>{lounge.name}</LoungeName>
-                      <LoungeOutButton>탈퇴</LoungeOutButton>
+                      <LoungeOutButton
+                        onClick={() => {
+                          setLoungeOutModalShow(true);
+                        }}
+                      >
+                        탈퇴
+                      </LoungeOutButton>
                     </LoungeRow>
                   ))}
                 </LoungeInfo>
               </LoungeBox>
-              <WithdrawalButton>계정삭제</WithdrawalButton>
+              <WithdrawalButton
+                onClick={() => {
+                  setWithdrawalModalShow(true);
+                }}
+              >
+                계정삭제
+              </WithdrawalButton>
             </InfoBox>
           </MyInfoModal>
+        )}
+        {/* //소속 라운지 탈퇴 모달 */}
+        {LoungeOutModalShow && (
+          <ConfirmModal
+            size="medium"
+            title="소속 라운지 탈퇴"
+            onClose={() => {
+              setLoungeOutModalShow(false);
+            }}
+            onConfirm={() => {
+              setLoungeOutModalShow(false);
+            }}
+          >
+            정말 [라운지이름]을 탈퇴하시겠습니까?
+          </ConfirmModal>
+        )}
+        {/* //소속 라운지 탈퇴 모달 */}
+        {WithdrawalModalShow && (
+          <ConfirmModal
+            size="medium"
+            title="계정 삭제"
+            onClose={() => {
+              setWithdrawalModalShow(false);
+            }}
+            onConfirm={() => {
+              setWithdrawalModalShow(false);
+            }}
+          >
+            <p>정말로 라이언타운과 작별 인사를 하시겠습니까?</p>
+            <LastAlertText>*추후 계정 복구는 불가능합니다.</LastAlertText>
+          </ConfirmModal>
         )}
       </MypageBg>
     </BackgroundMain>
@@ -101,18 +141,11 @@ const InfoBox = styled.div`
 `;
 
 // 이름 정보
-const NameBox = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-`;
 
 const NameTitle = styled.p`
-  display: flex;
-  justify-content: left;
   font-size: 12px;
   line-height: 18px;
-  margin-left: 12px;
+  margin-right: 260px;
 `;
 
 const NameInfo = styled.div`
@@ -123,17 +156,18 @@ const NameInfo = styled.div`
   flex-direction: row;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 `;
 
 const NameText = styled.p`
   font-size: 15px;
-  margin: 8px 0 8px 13px;
+  margin: 8px 0 7px 13px;
 `;
 
 const NameSaveButton = styled.button`
   color: ${({ theme }) => theme.colors.primary.green};
   font-size: 12px;
-  margin: 8px 12px 8px 0;
+  margin: 8px 12px 7px 0;
 `;
 
 // 라운지 정보
@@ -177,11 +211,16 @@ const LoungeOutButton = styled.button`
   margin: 8px 12px 3px 0;
 `;
 
-// 회원 탈퇴 버튼
+// 계정 삭제 버튼
 const WithdrawalButton = styled.button`
   color: #bd2517;
   font-size: 15px;
   margin: 14px 0 8px 220px;
+`;
+
+const LastAlertText = styled.p`
+  font-size: 12px;
+  margin: 10px 0;
 `;
 
 export default MyPage;
