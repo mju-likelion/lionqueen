39db9adbf2862c71bafe4e26ab1d171600574866
qkrthdus605 <qkrthdus605@gmail.com>
2022-06-29 00:00:58 +0900

    feat: key props 추가

---
+++ src/components/BubbleButton.tsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import Bubble from '../components/icons/Bubble';
+import Left from '../components/icons/LeftArrow';
+import Right from '../components/icons/RightArrow';
+
+const Button = () => {
+  type GroupType = { id: number; name: string };
+
+  const [group, setGroup] = useState<GroupType[]>([
+    {
+      id: 1,
+      name: '그룹이름1',
+    },
+    {
+      id: 2,
+      name: '그룹이름2',
+    },
+    {
+      id: 3,
+      name: '그룹이름3',
+    },
+  ]);
+
+  const groupList = group.map(n => (
+    <BubbleWrapper>
+      <Bubble />
+      <GroupName key={n.id}>{n.name}</GroupName>
+    </BubbleWrapper>
+  ));
+
+  return (
+    <Container>
+      <ArrowWrapper>
+        <Left />
+      </ArrowWrapper>
+      {groupList}
+      <ArrowWrapper>
+        <Right />
+      </ArrowWrapper>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  height: 124px;
+  width: 612px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 65px 0 60px 0;
+`;
+
+const BubbleWrapper = styled.div`
+  width: 124px;
+  height: 124px;
+  cursor: pointer;
+`;
+
+const ArrowWrapper = styled.div`
+  width: 49px;
+  height: 55px;
+  cursor: pointer;
+`;
+
+const GroupName = styled.p`
+  font-size: 20px;
+  color: ${({ theme }) => theme.colors.primary.black};
+  position: absolute;
+  left: 300px;
+  top: 400px;
+`;
+
+export default Button;
