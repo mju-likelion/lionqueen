3dc8e8ba1988749b10c34f3950ebe1f701186bb5
qkrthdus605 <qkrthdus605@gmail.com>
2022-07-02 23:52:13 +0900

    style: bubble svg 변경 및 yarn 재설치

--- src/components/icons/Bubble.tsx
+++ src/components/icons/Bubble.tsx
@@ -4,42 +4,42 @@ type Props = {
   color?: string;
 };
 
-const Bubble = ({ width = '133', height = '133', color = '#C5E5FF' }: Props) => {
+const Bubble = ({ width = '163', height = '163', color = '#C5E5FF' }: Props) => {
   return (
     <svg
       width={width}
       height={height}
-      viewBox="0 0 133 133"
+      viewBox="0 0 163 163"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g opacity="0.8" filter="url(#filter0_d_899_49)">
-        <circle cx="66.4639" cy="62.9365" r="62" fill={color} />
+      <g opacity="0.8" filter="url(#filter0_d_1165_51)">
+        <circle cx="81.5" cy="77.5" r="77.5" fill={color} />
       </g>
-      <ellipse cx="43.4761" cy="90.3871" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="31.8399" cy="71.5497" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="31.2574" cy="70.2504" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="30.6753" cy="68.9516" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="30.0938" cy="67.0024" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="29.5118" cy="65.0536" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="28.9302" cy="61.1562" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="28.9302" cy="57.2587" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="28.9302" cy="53.3617" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="29.5118" cy="49.4638" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="30.6753" cy="45.5668" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="31.8399" cy="42.3188" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="34.1661" cy="39.0707" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="34.7486" cy="38.4218" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="35.9121" cy="36.4726" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="37.6578" cy="35.1738" rx="8.72709" ry="9.74358" fill="white" />
-      <ellipse cx="39.9839" cy="33.8744" rx="8.72709" ry="9.74358" fill="white" />
+      <ellipse cx="53.9783" cy="112.75" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="39.9895" cy="89.5079" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="39.2893" cy="87.9044" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="38.5892" cy="86.3019" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="37.8904" cy="83.8971" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="37.1902" cy="81.4923" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="36.4915" cy="76.6837" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="36.4915" cy="71.8746" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="36.4915" cy="67.0665" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="37.1902" cy="62.2569" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="38.5892" cy="57.4488" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="39.9895" cy="53.441" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="42.7859" cy="49.4337" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="43.4861" cy="48.6329" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="44.8846" cy="46.2276" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="46.9837" cy="44.6251" rx="10.4915" ry="12.0221" fill="white" />
+      <ellipse cx="49.7806" cy="43.0221" rx="10.4915" ry="12.0221" fill="white" />
       <defs>
         <filter
-          id="filter0_d_899_49"
-          x="0.463867"
-          y="0.936523"
-          width="132"
-          height="132"
+          id="filter0_d_1165_51"
+          x="0"
+          y="0"
+          width="163"
+          height="163"
           filterUnits="userSpaceOnUse"
           colorInterpolationFilters="sRGB"
         >
@@ -54,11 +54,11 @@ const Bubble = ({ width = '133', height = '133', color = '#C5E5FF' }: Props) => 
           <feGaussianBlur stdDeviation="2" />
           <feComposite in2="hardAlpha" operator="out" />
           <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
-          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_899_49" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1165_51" />
           <feBlend
             mode="normal"
             in="SourceGraphic"
-            in2="effect1_dropShadow_899_49"
+            in2="effect1_dropShadow_1165_51"
             result="shape"
           />
         </filter>
