61a447199b06359b5532872bce4105607d564cd1
YeonBeen Park <ypyp66@naver.com>
2022-07-08 10:56:21 +0900

    fix: Icons로 변경

--- src/DesignSystem/BackgroundMain/index.tsx
+++ src/DesignSystem/BackgroundMain/index.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import Tree from '~components/icons/Tree';
+import Tree from '~components/Icons/Tree';
 
 type BackgroundProps = {
   children: React.ReactNode;
