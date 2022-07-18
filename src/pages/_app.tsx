1f256a749e094deea8f3114c683c0df09ccc2813
seongyeon <ysy2565@naver.com>
2022-07-19 02:03:13 +0900

    setting: recoil 삭제

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,15 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { RecoilRoot } from 'recoil';
 import { wrapper } from '../store';
 import { theme } from '~/styles/theme';
 
-import '../styles/globals.css';
-import Notice from '~components/Notice';
+import '~styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-        <Notice />
-      </ThemeProvider>
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
 
