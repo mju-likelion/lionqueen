8b37e89a972e897200eb576e5d053be61e2b6d01
seongyeon <ysy2565@naver.com>
2022-07-25 00:23:34 +0900

    setting: queryfn default 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider, QueryClient, QueryFunction } from '@tanstack/react-query';
 import { wrapper } from '../store';
 import { theme } from '~/styles/theme';
 
 import '~styles/globals.css';
+import Axios from '~lib/axios';
 
-const queryClient = new QueryClient();
+const defaultQueryFn: QueryFunction<unknown, readonly unknown[]> = async ({ queryKey }) => {
+  const { data } = await Axios.get(queryKey[0] as string);
+  return data;
+};
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: defaultQueryFn,
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
