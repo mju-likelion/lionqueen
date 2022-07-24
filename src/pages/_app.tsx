31a6410700195a4750c096604e44b80928cbc64a
seongyeon <ysy2565@naver.com>
2022-07-24 23:21:23 +0900

    setting: reactquery설치 및 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,19 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { wrapper } from '../store';
 import { theme } from '~/styles/theme';
 
 import '~styles/globals.css';
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
