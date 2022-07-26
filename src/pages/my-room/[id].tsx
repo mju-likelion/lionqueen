4412696996de282cad68a18e73a7fcf658725d43
Bhanjo <hanjo1515@naver.com>
2022-07-27 01:02:19 +0900

    refactor: react query로 데이터 패치 리팩터링

--- src/pages/my-room/[id].tsx
+++ src/pages/my-room/[id].tsx
@@ -1,43 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import styled from 'styled-components';
+import { useQuery } from '@tanstack/react-query';
 import Avatar from '~components/MyRoom/Avatar';
 import Board from '~components/MyRoom/Board';
 import GuestBook from '~components/MyRoom/GuestBook';
 import MemoModal from '~components/MyRoom/MemoModal';
 import { Comment } from '~/lib/commentType';
-import Axios from '~lib/axios';
 
 const MyRoom = () => {
   const router = useRouter();
   const [isModalPopup, setIsModalPopup] = useState(false);
   const [isSecondModalPopup, setIsSecondModalPopup] = useState(false);
   const [clickedMemoProps, setClickedMemoProps] = useState<Comment | null>(null);
-  const [myRoomData, setMyRoomData] = useState<any>({
-    roomData: {
-      id: 2,
-      createAt: '2022-07-22T22:51:44.739Z',
-      updateAt: '2022-07-22T22:51:44.739Z',
-      user: {
+
+  const { status, data, error, isFetching } = useQuery([`/api/rooms/${router.query.id}`]);
+
+  // api 완성 전 임시 동작, api 완성 후 status(에러,로딩,완료)에 따른 동작 변경 필요
+  let roomData: any = data;
+
+  if (roomData === undefined) {
+    console.log('데이터가 없습니다. 임시 데이터를 적용합니다.');
+    roomData = {
+      roomData: {
         id: 2,
-        phone: '01093202207',
-        name: '박재민',
-        email: 'pjm2207@naver.com',
-        createAt: '2022-07-22T22:45:51.374Z',
-        updateAt: '2022-07-22T22:45:51.374Z',
-        password1: null,
+        createAt: '2022-07-22T22:51:44.739Z',
+        updateAt: '2022-07-22T22:51:44.739Z',
+        user: {
+          id: 2,
+          phone: '01093202207',
+          name: '에러유저',
+          email: 'error2022@naver.com',
+          createAt: '2022-07-22T22:45:51.374Z',
+          updateAt: '2022-07-22T22:45:51.374Z',
+          password1: null,
+        },
       },
-    },
-    memos: [
-      {
-        id: 1,
-        title: '재민아',
-        content: '일좀해라',
-        createdAt: '2022-07-22T22:53:23.863Z',
-        updatedAt: '2022-07-22T22:53:37.218Z',
-      },
-    ],
-  });
+      memos: [
+        {
+          id: 1,
+          title: 'RQ에러',
+          content: '해결해줘',
+          createdAt: '2022-07-22T22:53:23.863Z',
+          updatedAt: '2022-07-22T22:53:37.218Z',
+        },
+      ],
+    };
+  }
 
   const handleRouteClick = () => {
     // 추후 라운지 내부로 가는 코드로 고치기
@@ -48,31 +57,13 @@ const MyRoom = () => {
     setIsModalPopup(!isModalPopup);
   };
 
-  const fetchData = async (id: string | string[] | undefined) => {
-    try {
-      const data = await Axios.get(`/api/myroom/${id}`);
-      if (data !== null) {
-        setMyRoomData(data.data);
-      }
-    } catch {
-      console.log('데이터 패치 실패');
-    }
-  };
-
   // 메모장 모달을 띄워줌과 동시에 메모장 props 세팅
   const handleSecondModalClick = (id: number | null) => {
     setIsSecondModalPopup(!isSecondModalPopup);
-    if (typeof myRoomData !== undefined) {
-      setClickedMemoProps(myRoomData.memos.find((item: Comment) => item.id === id) || null);
+    if (typeof roomData !== undefined) {
+      setClickedMemoProps(roomData.memos.find((item: Comment) => item.id === id) || null);
     }
   };
-
-  useEffect(() => {
-    // 라우터가 준비 됐을 때 데이터 패치
-    if (!router.isReady) return;
-    const routerId = router.query.id;
-    fetchData(routerId);
-  }, [router.isReady]);
 
   return (
     <Wrap>
@@ -80,7 +71,7 @@ const MyRoom = () => {
         <GuestBook
           onClose={() => setIsModalPopup(false)}
           handleSecondModalClick={handleSecondModalClick}
-          comments={myRoomData.memos}
+          comments={roomData.memos}
         />
       )}
       {isSecondModalPopup && (
@@ -88,14 +79,14 @@ const MyRoom = () => {
       )}
       <ContentContainer>
         {/* 여기에 사용자 이름 패치 필요 */}
-        <RoomName>사용자이름의 방</RoomName>
+        <RoomName>{roomData.roomData.user.name}</RoomName>
         <RoomConent>
           <Board
-            comments={myRoomData.memos}
+            comments={roomData.memos}
             handleModalClick={handleModalClick}
             handleSecondModalClick={handleSecondModalClick}
           />
-          <Avatar />
+          <Avatar roomOwner={roomData.roomData.user.name} />
         </RoomConent>
         <RoomBottom>
           <Door onClick={handleRouteClick} />
