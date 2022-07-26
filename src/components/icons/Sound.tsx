2b3c270b8e67aa2bf8e4a116feab72198ddca526
qkrthdus605 <qkrthdus605@gmail.com>
2022-07-26 14:44:23 +0900

    style: 확성기 이미지 추가

---
+++ src/components/icons/Sound.tsx
@@ -0,0 +1,155 @@
+type Props = {
+  width?: string;
+  height?: string;
+  color?: string;
+};
+
+const Plus = ({ width = '88', height = '69', color = '#ff93ad' }: Props) => {
+  return (
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 88 69"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.89551 29.6927L28.5731 16.9673L29.1122 58.2543L8.89551 51.4674V29.6927Z"
+        fill="#797979"
+      />
+      <g filter="url(#filter0_d_1643_682)">
+        <ellipse cx="30.7294" cy="37.6104" rx="9.1649" ry="21.2091" fill="#797979" />
+      </g>
+      <ellipse cx="32.3466" cy="37.3283" rx="4.85201" ry="15.8361" fill="#a7a7a7" />
+      <ellipse cx="15.674" cy="6.00661" rx="4.66959" ry="6.00661" fill={color} />
+      <ellipse
+        rx="4.80518"
+        ry="5.83713"
+        transform="matrix(0.0273481 0.999626 -0.999581 0.0289593 9.96617 10.9793)"
+        fill={color}
+      />
+      <ellipse
+        rx="4.75894"
+        ry="5.89602"
+        transform="matrix(0.576144 0.817348 -0.801361 0.59818 11.7587 17.0269)"
+        fill={color}
+      />
+      <ellipse
+        rx="4.73912"
+        ry="5.92091"
+        transform="matrix(-0.690547 0.723288 -0.703219 -0.710973 19.3162 17.2474)"
+        fill={color}
+      />
+      <ellipse
+        rx="4.80438"
+        ry="5.83816"
+        transform="matrix(0.0801607 0.996782 -0.996393 0.084854 21.2926 10.3898)"
+        fill={color}
+      />
+      <ellipse cx="15.6738" cy="12.0134" rx="1.45925" ry="1.50165" fill="white" />
+      <ellipse cx="16.1261" cy="56.9864" rx="3.00017" ry="3.82237" fill={color} />
+      <ellipse
+        rx="3.05785"
+        ry="3.75027"
+        transform="matrix(0.0276113 0.999619 -0.999588 0.0286832 12.459 60.1506)"
+        fill={color}
+      />
+      <ellipse
+        rx="3.03812"
+        ry="3.77524"
+        transform="matrix(0.579833 0.814735 -0.804098 0.594497 13.6107 63.9989)"
+        fill={color}
+      />
+      <ellipse
+        rx="3.02968"
+        ry="3.78582"
+        transform="matrix(-0.694003 0.719973 -0.706619 -0.707594 18.4664 64.1402)"
+        fill={color}
+      />
+      <ellipse
+        rx="3.0575"
+        ry="3.7507"
+        transform="matrix(0.0809277 0.99672 -0.996461 0.0840502 19.7361 59.7758)"
+        fill={color}
+      />
+      <ellipse cx="16.1263" cy="60.8091" rx="0.937552" ry="0.955592" fill="white" />
+      <ellipse cx="44.9871" cy="15.8944" rx="1.82153" ry="2.3207" fill={color} />
+      <ellipse
+        rx="1.85653"
+        ry="2.27695"
+        transform="matrix(0.0276116 0.999619 -0.999588 0.0286829 42.7606 17.8152)"
+        fill={color}
+      />
+      <ellipse
+        rx="1.84456"
+        ry="2.2921"
+        transform="matrix(0.579837 0.814732 -0.804101 0.594493 43.4596 20.1521)"
+        fill={color}
+      />
+      <ellipse
+        rx="1.83944"
+        ry="2.29852"
+        transform="matrix(-0.694006 0.719969 -0.706623 -0.707591 46.4078 20.2375)"
+        fill={color}
+      />
+      <ellipse
+        rx="1.85632"
+        ry="2.27721"
+        transform="matrix(0.0809285 0.99672 -0.996462 0.0840494 47.1789 17.5875)"
+        fill={color}
+      />
+      <ellipse cx="44.987" cy="18.2149" rx="0.569228" ry="0.580174" fill="white" />
+      <rect y="28.8442" width="9.1649" height="23.1886" rx="3" fill="#929292" />
+      <path
+        d="M48.6302 27.601L49.1657 27.828L49.9734 28.2851L50.2459 28.5198L50.7907 28.9893L51.0631 29.2241L51.608 29.6935L52.9798 31.1144L53.5342 31.831L54.0887 32.5477L54.6528 33.5115L55.2168 34.4753L55.7809 35.4392L56.0821 36.4153L56.2165 37.3279L56.3371 38.1764L56.3371 39.0247L56.3371 39.8731L56.3371 41.0042L56.2165 41.9676L55.8348 43.6078L55.1134 45.3752L54.3825 46.8954L53.1354 48.6877L52.1321 49.9731L50.8658 51.2711L49.59 52.3218"
+        stroke="#2e2e2e"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M59.9265 21.476L60.5788 21.8056L61.5643 22.4656L61.8977 22.8025L62.5644 23.4766L62.8978 23.8136L63.5646 24.4875L65.2461 26.5235L65.9276 27.5486L66.6091 28.5736L67.3052 29.9497L68.0014 31.3257L68.6975 32.702L69.075 34.092L69.1338 35.4962L69.1779 36.5493L69.2367 37.9534L69.2808 39.0066L69.0799 41.8289L68.8643 44.3001L68.011 46.7997L67.1434 48.948L65.6528 51.4756L64.4517 53.2869L62.9318 55.1124L61.3972 56.5868"
+        stroke="#2e2e2e"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M73.0153 18.7804L73.8228 19.1644L75.0421 19.9346L75.4542 20.3286L76.2783 21.1168L76.6903 21.5108L77.5145 22.2988L79.5915 24.681L80.4324 25.881L81.2734 27.081L82.1311 28.6929L82.9889 30.3048L83.8467 31.917L84.3091 33.5466L84.5449 35.3486L84.6406 36.4798L84.6406 38.1765L84.5449 39.3135L84.2844 42.6267L84.0072 45.528L82.9393 48.4652L81.8548 50.9903L79.9964 53.9632L78.4998 56.0943L76.6078 58.2434L74.6991 59.9804"
+        stroke="#2e2e2e"
+        strokeWidth="5"
+        strokeLinecap="round"
+      />
+      <defs>
+        <filter
+          id="filter0_d_1643_682"
+          x="17.5645"
+          y="16.4014"
+          width="26.3298"
+          height="50.418"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1643_682" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_1643_682"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
+
+export default Plus;
