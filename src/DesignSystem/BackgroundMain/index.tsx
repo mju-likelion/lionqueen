b01a7cc4066eee7491860773d8ce24416862d4a1
Bhanjo <hanjo1515@naver.com>
2022-07-28 19:10:26 +0900

    feat: 구름 애니메이션 구현

--- src/DesignSystem/BackgroundMain/index.tsx
+++ src/DesignSystem/BackgroundMain/index.tsx
@@ -8,10 +8,41 @@ type BackgroundProps = {
 
 function BackgroundMain(props: BackgroundProps) {
   const { children, ...restProps } = props;
+  const clouds = [
+    {
+      id: 0,
+      width: '194px',
+      height: '118px',
+      right: '0',
+    },
+    {
+      id: 1,
+      width: '316px',
+      height: '156px',
+      right: '50%',
+    },
+    {
+      id: 2,
+      width: '208px',
+      height: '116px',
+      right: '10%',
+    },
+  ];
 
   return (
     <PageContainer>
       <BackgroundBox>
+        <CloudBox>
+          {clouds.map(cloud => (
+            <img
+              src="/image/cloud.png"
+              key={cloud.id}
+              alt="구름"
+              width={cloud.width}
+              height={cloud.height}
+            />
+          ))}
+        </CloudBox>
         <Board>
           <LineContainer>
             <LineStyles />
@@ -43,13 +74,66 @@ const BackgroundBox = styled.div`
   justify-content: center;
   margin: 0 auto;
   box-shadow: none;
-  background-image: url('https://lionqueen.s3.amazonaws.com/background.png');
+
+  /* s3에 이미지 업로드 필요 */
+  background-image: url('image/backgoundNoCloud.png');
   background-position: center;
   background-repeat: no-repeat;
   background-size: 1440px 1024px;
   padding-top: 93px;
   min-width: 1440px;
   min-height: 1024px;
+`;
+
+const CloudBox = styled.div`
+  @keyframes cloud-flow {
+    0% {
+      right: 0;
+    }
+
+    10% {
+      opacity: 1;
+    }
+
+    90% {
+      opacity: 1;
+    }
+
+    100% {
+      right: 100%;
+      opacity: 0;
+    }
+  }
+
+  position: absolute;
+  top: 0;
+  min-width: 1440px;
+  min-height: 1024px;
+
+  img {
+    position: absolute;
+    right: 0;
+    opacity: 0;
+    animation: cloud-flow 1s linear infinite;
+  }
+
+  img:nth-child(1) {
+    top: 3%;
+    animation-duration: 12s;
+    animation-delay: 0;
+  }
+
+  img:nth-child(2) {
+    top: 25%;
+    animation-duration: 17s;
+    animation-delay: -0.8s;
+  }
+
+  img:nth-child(3) {
+    top: 45%;
+    animation-duration: 14s;
+    animation-delay: 1.5s;
+  }
 `;
 
 const Board = styled.div`
